034fa96e14622b9382f99501ddba7c9b9afab0bb
Ahmed <ahmed_hany19@outlook.com>
2023-01-14 21:32:37 +0200

    Axios Requests

--- axios.js
+++ axios.js
@@ -1,9 +1,25 @@
-const getBtn = document.getElementById('get-btn');
-const postBtn = document.getElementById('post-btn');
+const getBtn = document.getElementById("get-btn");
+const postBtn = document.getElementById("post-btn");
 
-const getData = () => {};
+const getData = () => {
+  axios.get("https://reqres.in/api/users").then((response) => {
+    console.log(response.data);
+  });
+};
 
-const sendData = () => {};
+const sendData = () => {
+  axios
+    .post("https://reqres.in/api/register", {
+      email: "eve.holt@reqres.in",
+      //       password: "pistol",
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-getBtn.addEventListener('click', getData);
-postBtn.addEventListener('click', sendData);
+getBtn.addEventListener("click", getData);
+postBtn.addEventListener("click", sendData);
